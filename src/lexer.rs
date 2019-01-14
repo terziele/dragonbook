acd3b5f0728b77e5ace3c3c820f5bf0840fd8c46
terziele <autushka.ihnat@gmail.com>
2019-01-14 22:26:53 +0300

    update lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -30,11 +30,80 @@ impl Token {
 
 use std::io::Read;
 use std::collections::HashMap;
-struct Lexer {
+pub struct Lexer {
     peek: char,
-    buf: [char;10],
+    buf: [u8;10],
+    buf_index: usize,
     reader: Box<dyn Read>,
     words: HashMap<String, Token>,
+    line: u64,
+}
+
+impl Lexer {
+    pub fn new(reader: Box<dyn Read>) -> Self {
+        let mut words = HashMap::new();
+        macro_rules! reserve_word {
+            ($word:expr, $tag:expr) => {
+                {
+                    let lexeme = $word.to_string();
+                    let word = Token::word($tag, lexeme.clone());
+                    words.insert(lexeme, word) ;
+                }
+            };
+        }
+
+        reserve_word!("true", tag::TRUE);
+        reserve_word!("false", tag::FALSE);
+
+        Self {
+            peek: ' ', 
+            buf: [0;10],
+            buf_index: 0,
+            reader,
+            words,
+            line: 0u64,
+        }
+    }
+
+    pub fn read(&mut self) -> Token {
+        loop {
+            let peek = self.next_char();
+
+            if peek == ' ' || peek == '\t' {
+                continue;
+            } else if peek == '\n' {
+                self.line += 1;
+            } else {
+                break;
+            }
+
+            if peek.is_digit(0) {
+                let val = 0;
+            }
+
+
+        }
+
+
+
+        Token::num(10)
+    }
+
+    fn next_char(&mut self) -> char {
+        if self.buf_index <  self.buf.len() {
+            self.peek = self.buf[self.buf_index] as char;
+            self.buf_index += 1;
+            return self.peek;
+        } else {
+            // fill the buffer
+            self.buf_index = 0;
+            self.reader.read(&mut self.buf);
+            if self.buf.is_empty() {
+                return '\0';
+            }
+            return self.next_char();
+        }
+    }
 }
 
 mod tag {
