58e3fd0cfbbf617b86d52e2b1e4214695b9f386b
terziele <autushka.ihnat@gmail.com>
2019-01-16 22:15:36 +0300

    remove build results

--- 2.6/target/debug/lexer-6554d780e2f9c8bc.d
+++
@@ -1,1 +0,0 @@
-/home/terziele/code/rust/dragonbook/target/debug/lexer-6554d780e2f9c8bc: /home/terziele/code/rust/dragonbook/src/lexer.rs /home/terziele/code/rust/dragonbook/src/main.rs
--- 2.6/target/debug/liblexer.d
+++
@@ -1,1 +0,0 @@
-/home/terziele/code/rust/dragonbook/target/debug/liblexer.rmeta: /home/terziele/code/rust/dragonbook/src/lexer.rs /home/terziele/code/rust/dragonbook/src/main.rs
